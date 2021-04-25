da41f37f94df3014b2233737528e023a7d57f34e
Jonas Blome <jonas.blome@gmx.de>
2021-04-25 12:53:35 +0200

    README.md

---
+++ tiled tutorial/super-mario.tsx
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="super-mario" tilewidth="16" tileheight="16" tilecount="44" columns="11">
+ <image source="../phaser tutorial/phaser3-tutorial-src/assets/super-mario.png" width="176" height="64"/>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.272727" width="15.6364" height="15.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.454545" y="0.454545" width="15.2727" height="15.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.363636" y="0.272727" width="15.4545" height="15.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.545455" width="15.6364" height="15.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="4">
+   <object id="6" x="0.272727" y="0.454545" width="15.3636" height="15.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="0.0909091" width="15.6364" height="15.6364"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.181818" width="15.7273" height="15.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.15625" y="0.1875" width="15.75" height="15.7188"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.36364" y="0.181818" width="13.1818" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.181818" y="0.363636" width="13.5455" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.545455" width="15.4545" height="15.1818"/>
+   <object id="2" x="15.7273" y="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.272727" width="15.3636" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.272727" y="0.272727" width="15.4545" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.181818" y="0.272727" width="15.7273" height="15.6364"/>
+  </objectgroup>
+ </tile>
+</tileset>
